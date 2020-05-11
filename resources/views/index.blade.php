bc9b15178ba42684e2356267365fb77fd474ba0a
may-tech <aehkloprsw@gmail.com>
2020-05-11 23:59:26 +0900

    [add] layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,30 @@
 
 @section('content')
   <a href="{{ route('create') }}">メモする</a>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent">
   @foreach($memos as $memo)
-  <div>
-    <span>{{ $memo->content }}</span>
-    <a href="{{ route('edit', ['id'=>$memo->id]) }}">書きなおす</a>
-    <a href="{{ route('delete', ['id'=>$memo->id]) }}">消す</a>
-  </div>
+      <div class="tile is-child box">
+
+        <div class="container">
+          <header>
+            <p class="card-header-title">{{ $memo->created_at }}</p>
+          </header>
+          <div class="content">
+              <span>{{ $memo->content }}</span>
+          </div>
+          <footer>
+            <!-- <a href="#" class="card-footer-item">Save</a>
+            <a href="#" class="card-footer-item">Edit</a> -->
+            <div class="buttons">
+               <button class="button is-link is-light"><a href="{{ route('edit', ['id'=>$memo->id]) }}">書きなおす</a></button>
+               <button class="button is-danger is-light"><a href="{{ route('delete', ['id'=>$memo->id]) }}">消す</a></button>
+            </div>
+          </footer>
+       </div>
+      </div>
   @endforeach
+    </div>
+  </div>  
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,93 +1,22 @@
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>MemoApp</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    MemoApp
-                </div>
-
-                <div>
-                    @yield('content')
-                </div>
-            </div>
-        </div>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <section class="section">
+    <div class="container has-text-centered">
+      <h1 class="title is-centered">May's Memo App</h1>
+      <p class="subtitle">My second App by Laravel</p>
+      <div>
+        @yield('content')
+      </div>
+    </div>
+  </section>
     </body>
 </html>
