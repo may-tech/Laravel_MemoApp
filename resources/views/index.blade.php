1ae8f79b795263180b3a2a701f68e129a343a6aa
may-tech <aehkloprsw@gmail.com>
2020-05-12 06:09:25 +0900

    [clean] layout to card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,48 @@
 @extends('layout')
 
 @section('content')
-  <a href="{{ route('create') }}">メモする</a>
-  <div class="tile is-ancestor">
-    <div class="tile is-parent">
+<button class="button is-info is-outlined">
+  <a href="{{ route('create') }}">
+    <i class="far fa-edit"></i>
+  </a>
+</button>
+
+<!-- <div class="tile is-ancestor is-vertical"> -->
   @foreach($memos as $memo)
-      <div class="tile is-child box">
-
-        <div class="container">
-          <header>
-            <p class="card-header-title">{{ $memo->created_at }}</p>
-          </header>
-          <div class="content">
-              <span>{{ $memo->content }}</span>
-          </div>
-          <footer>
-            <!-- <a href="#" class="card-footer-item">Save</a>
-            <a href="#" class="card-footer-item">Edit</a> -->
-            <div class="buttons">
-               <button class="button is-link is-light"><a href="{{ route('edit', ['id'=>$memo->id]) }}">書きなおす</a></button>
-               <button class="button is-danger is-light"><a href="{{ route('delete', ['id'=>$memo->id]) }}">消す</a></button>
-            </div>
-          </footer>
-       </div>
-      </div>
+  
   @endforeach
-    </div>
-  </div>  
 
+
+  @foreach($memos as $memo)
+  <!-- <div class="tile is-parent is-6">
+    <div class="tile is-child"> -->
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            {{ $memo->created_at }}
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            {{ $memo->content }}
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a href="{{ route('edit', ['id'=>$memo->id]) }}" class="card-footer-item has-background-info	has-text-white">
+            <span class="icon">
+              <i class="fas fa-screwdriver"></i>
+            </span>
+          </a>
+          <a href="{{ route('delete', ['id'=>$memo->id]) }}" class="card-footer-item has-background-light">
+          <span class="icon">
+            <i class="fa fa-times" aria-hidden="true"></i>
+            </span>
+          </a>
+        </footer>
+      </div>
+      <br>
+    <!-- </div>
+  </div> -->
+  @endforeach
+  <!-- </div> -->
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
+    <title>May's Memo App</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
   </head>
@@ -13,7 +13,7 @@
     <div class="container has-text-centered">
       <h1 class="title is-centered">May's Memo App</h1>
       <p class="subtitle">My second App by Laravel</p>
-      <div>
+      <div class="section">
         @yield('content')
       </div>
     </div>
